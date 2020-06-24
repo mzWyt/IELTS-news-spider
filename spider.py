f5010fc4f95d44b8fc1467ad7630ef3ec2aeb93b
Benjamin Hsu <mr_miser@163.com>
2020-06-24 23:36:26 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,7 +1,14 @@
+import urllib.request
+from bs4 import BeautifulSoup
 import requests
 
-url = 'https://weibo.com/ieltscn/'
+url = 'https://ielts.neea.edu.cn/'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
 
-res = requests.get(url)
-rslt = res.content.decode("utf8","ignore")
-print(rslt)
+res = requests.get(url, headers=headers)
+content = res.text
+soup = BeautifulSoup(content, 'html.parser')
+
+divs = soup.find_all(class_ = 'margin-top: -.2em;')
+
+print(divs)
